372bd92184f7b34370b662593ad0e9b054781e53
ajay-chavan5 <113526673+ajay-chavan5@users.noreply.github.com>
2024-06-27 14:48:13 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,15 +5,16 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World!";
-
+    private static final String MESSAGE1= "Hello World!";
+    private static final String MESSAGE2 = "Bye World!"
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println(MESSAGE);
+        System.out.println(MESSAGE1);
+        System.out.println(MESSAGE2)
     }
 
     public String getMessage() {
-        return MESSAGE;
+        return MESSAGE1,MESSAGE2;
     }
 }
